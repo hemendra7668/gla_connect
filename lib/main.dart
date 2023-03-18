4a1936aa07abb6271291417afba9b0ed8ddbcd32
Hemendra Kumar <hem.pachahara@gmail.com>
2023-03-18 13:10:37 +0530

    yess

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 
-  ///
-  ///
 
-  ///
 }
 
 class MyApp extends StatelessWidget {
@@ -15,6 +12,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+
+    );
        }
 }
